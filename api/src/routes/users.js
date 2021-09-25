40acab02f4d31b09297a23f2d1bc1f907f90f7f8
longle <longtle@kobiton.com>
2021-09-26 02:21:43 +0700

    connect db sucessfull

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import {create} from '../utils/debug'
-import db from '../services/core-services/db'
 
 const router = express.Router();
 
 const debug = create('dev')
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  debug.log(await db.Product.findAll());
   res.send('respond with a resource');
 });
 
